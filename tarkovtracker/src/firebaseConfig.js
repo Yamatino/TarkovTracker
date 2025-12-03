95bbaf935b3499bd8cfa758a1013dd59488ba3ea
Yamatino <yamatino@gmail.com>
2025-12-03 09:19:25 -0300

    Initialize Firebase SDK on multiplayer branch

---
+++ tarkovtracker/src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDmnyWsbuhksvNGydNTWreK9ojK8PIpARU",
+  authDomain: "tarkovtracker-squad.firebaseapp.com",
+  projectId: "tarkovtracker-squad",
+  storageBucket: "tarkovtracker-squad.firebasestorage.app",
+  messagingSenderId: "168303246246",
+  appId: "1:168303246246:web:ba0bc97b172ff362e56d67"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
